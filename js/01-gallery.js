ac74bad9c95fa4a241e22dbd1c954cd7c1f11dd0
Dmytro Gobosh <irulka103@ukr.net>
2021-12-20 23:31:45 +0200

    + gallery task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const gallery_js = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+gallery_js.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery_js.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  evt.target.src = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+      <img src="${evt.target.src}" width="800" height="800">
+  `);
+
+  instance.show();
+}
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
